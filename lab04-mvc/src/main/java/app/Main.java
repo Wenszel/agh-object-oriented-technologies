898929554f515453bd82c8eb7904a23a96d2d055
Wenszel <wiktor.smaga13@gmail.com>
2024-10-15 11:22:05 +0200

    add searching photos in lab04

--- lab04-mvc/src/main/java/app/Main.java
+++ lab04-mvc/src/main/java/app/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 public class Main {
 
-    public static final String SCRAPER_API_KEY = "*** Enter your https://www.scraperapi.com API key HERE ***";
+    public static final String SCRAPER_API_KEY = "73dd7ef3c57ab41963769ad52d946888";
 
 
     public static void main(String[] args) {
